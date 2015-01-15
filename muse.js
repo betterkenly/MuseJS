7c8792806888017047e6aef67d020287a61dd768
Jasna Todorovic <jasna@interaxon.ca>
2015-01-15 18:53:34 -0500

    add support for more OSC paths

--- muse.js
+++ muse.js
@@ -1,33 +1,105 @@
-$(function(window, $, undefined){
+$(function(window, undefined){
 
 	
 
 	window.Muse = window.Muse || {
-			on : {
+			relative : {
 				alpha: function(obj) {
-					Muse.on.brainwave('alpha', obj)
+					Muse.relative.brainwave('alpha', obj);
 				},
 				beta: function(obj) {
-					Muse.on.brainwave('beta', obj)
+					Muse.relative.brainwave('beta', obj);
 				},
 				delta: function(obj) {
-					Muse.on.brainwave('delta', obj)
+					Muse.relative.brainwave('delta', obj);
 				},
 				gamma: function(obj) {
-					Muse.on.brainwave('gamma', obj)
+					Muse.relative.brainwave('gamma', obj);
 				},
 				theta: function(obj) {
-					Muse.on.brainwave('theta', obj)
+					Muse.relative.brainwave('theta', obj);
 				},
                 'brainwave' : function( band, obj) {
-                },
-                'blink' : function( obj ){
                 }
-
-			},
-			connect: function(){
-
-			   var socket = io.connect('http://127.0.0.1', { port: 8081, rememberTransport: false, reconnect: true});
+            },
+            absolute: {
+            	low_freq: function(obj){
+            	},
+            	alpha: function(obj) {
+            		Muse.absolute.brainwave('alpha', obj);
+            	},
+            	beta: function(obj) {
+            		Muse.absolute.brainwave('beta', obj);
+            	},
+            	delta: function(obj) {
+            		Muse.absolute.brainwave('delta', obj);
+            	},
+            	gamma: function(obj) {
+            		Muse.absolute.brainwave('gamma', obj);
+            	},
+            	theta: function(obj) {
+            		Muse.absolute.brainwave('theta', obj);
+            	},
+            	brainwave : function( band, obj) {            	
+            	}
+            },
+            session: {
+            	alpha: function(obj){
+            		Muse.session.brainwave('alpha', obj);
+            	},
+            	beta: function(obj){
+            		Muse.session.brainwave('beta', obj);
+            	},
+            	delta: function(obj){
+            		Muse.session.brainwave('delta', obj);
+            	},
+            	gamma: function(obj){
+            		Muse.session.brainwave('gamma', obj);
+            	},
+            	theta: function(obj){
+            		Muse.session.brainwave('theta', obj);
+            	},
+            	brainwave: function(band, obj){
+            	}
+            },
+            experimental: {
+            	mellow: function (obj){            		
+            	},
+            	concentration: function(obj){            		
+            	}
+            },
+            muscle: {
+            	'blink' : function( obj ){
+            	},
+            	'jaw' : function( obj ){            		
+            	}
+            },
+            raw: {
+            	fft0: function ( obj ){
+            	},
+            	fft1: function ( obj ){
+            	},
+            	fft2: function ( obj ){
+            	},
+            	fft3: function ( obj ){
+            	}
+            },
+            accelerate : function( obj ){                	
+            },
+			connect: function( opts ){
+				var defaults = {
+					host: 'http://127.0.0.1',
+					port: 8081,
+				    socket: {
+				        host: '127.0.0.1',
+				        ports: {
+				          client: 3334,
+				          server: 3333
+				        }
+				    }
+				},
+				opts = opts || defaults,
+				socket = io.connect(opts.host, { port: opts.port, rememberTransport: false, reconnect: true});
 
 			   socket.on('connect', function() {
 			        // sends to socket.io server the host/port of oscServer
@@ -35,12 +107,12 @@ $(function(window, $, undefined){
 			        socket.emit('config',
 			            {
 			                server: {
-			                    port: 3333,
-			                    host: '127.0.0.1'
+			                    port: opts.socket.ports.server,
+			                    host: opts.socket.host
 			                },
 			                client: {
-			                    port: 3334,
-			                    host: '127.0.0.1'
+			                    port: opts.socket.ports.client,
+			                    host: opts.socket.host
 			                }
 			            }
 			        );
@@ -54,25 +126,83 @@ $(function(window, $, undefined){
 			},
             _handle: {
                 '/muse/elements/alpha_relative' : function(obj){
-                    Muse.on.alpha(obj);
+                    Muse.relative.alpha(obj);
                 },
                 '/muse/elements/beta_relative' : function(obj){
-                    Muse.on.beta(obj);
+                    Muse.relative.beta(obj);
                 },
                 '/muse/elements/delta_relative' : function(obj){
-                    Muse.on.delta(obj);
+                    Muse.relative.delta(obj);
                 },
                 '/muse/elements/gamma_relative' : function(obj){
-                    Muse.on.gamma(obj);
+                    Muse.relative.gamma(obj);
                 },
                 '/muse/elements/theta_relative' : function(obj){
-                    Muse.on.theta(obj);
+                    Muse.relative.theta(obj);
+                },
+                '/muse/elements/low_freqs_absolute' : function(obj){
+                	Muse.absolute.low_freq(obj);
+                },
+                '/muse/elements/alpha_absolute' : function(obj){
+                    Muse.absolute.alpha(obj);
+                },
+                '/muse/elements/beta_absolute' : function(obj){
+                    Muse.absolute.beta(obj);
+                },
+                '/muse/elements/delta_absolute' : function(obj){
+                    Muse.absolute.delta(obj);
+                },
+                '/muse/elements/gamma_absolute' : function(obj){
+                    Muse.absolute.gamma(obj);
+                },
+                '/muse/elements/theta_absolute' : function(obj){
+                    Muse.absolute.theta(obj);
+                },
+                '/muse/elements/delta_session_score' : function(obj){
+                	Muse.session.delta(obj);
+                },
+                '/muse/elements/theta_session_score' : function(obj){
+                	Muse.session.theta(obj);
+                },
+                '/muse/elements/alpha_session_score' : function(obj){
+                	Muse.session.alpha(obj);
+                },
+                '/muse/elements/beta_session_score' : function(obj){
+                	Muse.session.beta(obj);
+                },
+                '/muse/elements/gamma_session_score' : function(obj){
+                	Muse.session.gamma(obj);
                 },
                 '/muse/elements/blink' : function(obj){
-                	Muse.on.blink(obj);
+                	Muse.muscle.blink(obj);
+                },
+                '/muse/elements/jaw_clench' : function(obj){
+                	Muse.muscle.jaw(obj);
+                },
+                '/muse/acc' : function(obj){
+                	Muse.accelerate(obj);
+                },
+                '/muse/elements/experimental/mellow' : function(obj){
+                	Muse.experimental.mellow(obj);
+                },
+                '/muse/elements/experimental/concentration' : function(obj){
+                	Muse.experimental.concentration(obj);
+                },
+                '/muse/elements/raw_fft0' : function(obj){
+                	Muse.raw.fft0(obj);
+                },
+                '/muse/elements/raw_fft1' : function(obj){
+                	Muse.raw.fft1(obj);
+                },
+                '/muse/elements/raw_fft2' : function(obj){
+                	Muse.raw.fft2(obj);
+                },
+                '/muse/elements/raw_fft3' : function(obj){
+                	Muse.raw.fft3(obj);
                 }
+
             }
 
 	}
 
-}(window, jQuery))
+}(window))
